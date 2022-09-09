2846b253b073070ca1598c7e8388e51255ffc118
lintian <lintian@cs.unc.edu>
2022-09-09 11:33:07 -0400

    Update the script to run ablation study on GradICON.

--- training_scripts/gradICON_lung_pipeline/experiment_list.sh
+++ training_scripts/gradICON_lung_pipeline/experiment_list.sh
@@ -1,12 +1,14 @@
-python halfres_train_lung.py --exp="gradICON_aug_lncc" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="GradientICON" --augmentation=1 --sim="LNCC" --lamda=1
-python halfres_train_lung.py --exp="gradICON_aug_adaNCC" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="GradientICON" --augmentation=1 --sim="AdaptiveNCC" --lamda=0.1
-python halfres_train_lung.py --exp="ICON_aug_LNCC" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="InverseConsistentNet" --augmentation=1 --sim="LNCC" --lamda=700
-python halfres_train_lung.py --exp="ICON_aug_adaNCC" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="InverseConsistentNet" --augmentation=1 --sim="AdaptiveNCC" --lamda=700
+output_root=""
+dataset_root=""
 
-python halfres_train_lung.py --exp="GradICON_aug_NCC" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="GradientICON" --augmentation=1 --sim="NCC" --lamda=1
+# Comparison between LNCC, AdaptiveNCC and NCC
+python halfres_train_lung.py --exp="GradICON_noaug_LNCC" --output_root="$output_root" --dataset_root="$dataset_root" --reg="GradientICON" --augmentation=0 --sim="LNCC" --lamda=1
+python halfres_train_lung.py --exp="GradICON_noaug_adaNCC" --output_root="$output_root" --dataset_root="$dataset_root" --reg="GradientICON" --augmentation=0 --sim="AdaptiveNCC" --lamda=1
+python halfres_train_lung.py --exp="GradICON_noaug_NCC" --output_root="$output_root" --dataset_root="$dataset_root" --reg="GradientICON" --augmentation=0 --sim="NCC" --lamda=1
 
+# Comparison between augmentation vs no augmentation
+# python halfres_train_lung.py --exp="gradICON_aug_lncc" --output_root="$output_root" --dataset_root="$dataset_root" --reg="GradientICON" --augmentation=1 --sim="LNCC" --lamda=1
+# python halfres_train_lung.py --exp="gradICON_aug_adaNCC" --output_root="$output_root" --dataset_root="$dataset_root" --reg="GradientICON" --augmentation=1 --sim="AdaptiveNCC" --lamda=1
 
-python halfres_train_lung.py --exp="GradICON_noaug_LNCC" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="GradientICON" --augmentation=0 --sim="LNCC" --lamda=1
-python halfres_train_lung.py --exp="GradICON_noaug_adaNCC" --output_root="results/" --dataset_root="/playpen-ssd/tgreer/ICON_lung/results/half_res_preprocessed_transposed_SI/" --reg="GradientICON" --augmentation=0 --sim="AdaptiveNCC" --lamda=.1
-
-
+# Comparison between ICON and gradICON
+# python halfres_train_lung.py --exp="ICON_aug_LNCC" --output_root="$output_root" --dataset_root="$dataset_root" --reg="InverseConsistentNet" --augmentation=1 --sim="LNCC" --lamda=700
