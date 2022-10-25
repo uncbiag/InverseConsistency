20f6a45a34cd363d88c4ffe7bffdbfc3d847e767
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2022-10-25 13:18:21 -0400

    abdomen eval fixes

--- training_scripts/cvpr_clean/l2r_abdomen_eval.py
+++ training_scripts/cvpr_clean/l2r_abdomen_eval.py
@@ -13,7 +13,7 @@ parser.add_argument("--data_folder", type=str, help="the path to the folder cont
 parser.add_argument("--output_folder", type=str, help="the path to the folder the computed displacement will be saved")
 
 input_shape = [1, 1, 256, 160, 192]
-clamp = [-1000, 0]
+clamp = [-200, 500]
 device = torch.device('cuda:0')
 
 args = parser.parse_args()
@@ -22,7 +22,7 @@ footsteps.initialize(output_root=args.output_folder)
 
 import utils
 
-net = cvpr_network.make_network(input_shape, include_last_step=False)
+net = cvpr_network.make_network(input_shape, include_last_step=True)
 
 utils.log(net.regis_net.load_state_dict(torch.load(weights_path), strict=False))
 net.to(device)
@@ -43,20 +43,20 @@ for (fixed_path, moving_path) in test_cases:
     moving = np.asarray(itk.imread(os.path.join(args.data_folder, moving_path)))
 
     fixed = torch.Tensor(np.array(fixed)).unsqueeze(0).unsqueeze(0)
-    fixed = (torch.clamp(fixed, clamp[0], clamp[1]) + clamp[0])/(clamp[1]-clamp[0])
+    fixed = (torch.clamp(fixed, clamp[0], clamp[1]) - clamp[0])/(clamp[1]-clamp[0])
     
     moving = torch.Tensor(np.array(moving)).unsqueeze(0).unsqueeze(0)
-    moving = (torch.clamp(moving, clamp[0], clamp[1]) + clamp[0])/(clamp[1]-clamp[0])
+    moving = (torch.clamp(moving, clamp[0], clamp[1]) - clamp[0])/(clamp[1]-clamp[0])
 
     with torch.no_grad():
         net(fixed.to(device), moving.to(device))
 
         # phi_AB and phi_BA are [1, 3, H, W, D] pytorch tensors representing the forward and backward
         # maps computed by the model
-        phi_AB = net.phi_AB(net.identity_map)
+        phi_BA = net.phi_BA(net.identity_map)
 
         # Transform to displacement format that l2r evaluation script accepts
-        disp = (phi_AB - net.identity_map)[0].cpu()
+        disp = (phi_BA- net.identity_map)[0].cpu()
 
         network_shape_list = list(net.identity_map.shape[2:])
 
