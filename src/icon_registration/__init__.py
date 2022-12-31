0ab80e9a2b6db3b6dc387a89a193282ff3a4a9a9
Lin Tian <lintian@cs.unc.edu>
2022-12-31 13:06:51 -0500

    Refactor all the similarity loss to inherient from SimilarityBase. SimilarityBase has a member variable called isInterpolated and it indicates whether the similarity loss class requires mask for the interpolated evaluation.

--- src/icon_registration/__init__.py
+++ src/icon_registration/__init__.py
@@ -5,8 +5,9 @@ from icon_registration.losses import (
     GradientICON,
     InverseConsistentNet,
     gaussian_blur,
-    ssd_only_interpolated,
-    ssd,
+    SSDOnlyInterpolated,
+    SSD,
+    NCC
 )
 from icon_registration.network_wrappers import (
     DownsampleRegistration,
