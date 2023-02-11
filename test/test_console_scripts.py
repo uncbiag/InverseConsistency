fdcebee3c0bfdbd2fb66ef7698e0fe21df1517b8
HastingsGreer <hastings.greer@gmail.com>
2023-02-11 12:21:36 -0500

    Create test_console_scripts.py

---
+++ test/test_console_scripts.py
@@ -0,0 +1,20 @@
+import unittest
+import subprocess
+import icon_registration.test_utils
+
+class TestConsoleScripts(unittest.TestCase):
+    def test_register_pair_script(self):
+        test_utils.download_test_data()
+        subprocess.run(
+          [
+            "icon_register_pair",
+            "--fixed_image", 
+            test_utils.TEST_DATA_DIR / "brain_test_data" / "2_T1w_acpc_dc_restore_brain.nii.gz",
+            "--moving_image", 
+            test_utils.TEST_DATA_DIR / "brain_test_data" / "8_T1w_acpc_dc_restore_brain.nii.gz",
+            "--model",
+            "icon_registration.pretrained_models.brain_registration_model"
+            "--warped_image_out", 
+            "warped.nii.gz"
+          ]
+        )
