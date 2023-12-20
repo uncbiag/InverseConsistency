452764fa3850003fca12e7d5943e056f18512dd2
Ariel H. Curiale <curiale@gmail.com>
2023-12-20 09:05:10 -0500

    GradICON requires the itk_wrapper

--- src/icon_registration/__init__.py
+++ src/icon_registration/__init__.py
@@ -1,16 +1,7 @@
-from icon_registration.losses import (
-    LNCC,
-    LNCCOnlyInterpolated,
-    BlurredSSD,
-    GradientICON,
-    InverseConsistentNet,
-    gaussian_blur,
-    ssd_only_interpolated,
-    ssd,
-    SSDOnlyInterpolated,
-    SSD,
-    NCC
-)
+from icon_registration.losses import (LNCC, LNCCOnlyInterpolated, BlurredSSD,
+                                      GradientICON, InverseConsistentNet,
+                                      gaussian_blur, ssd_only_interpolated,
+                                      ssd, SSDOnlyInterpolated, SSD, NCC)
 from icon_registration.network_wrappers import (
     DownsampleRegistration,
     FunctionFromMatrix,
@@ -19,3 +10,5 @@ from icon_registration.network_wrappers import (
     TwoStepRegistration,
 )
 from icon_registration.train import train_batchfunction, train_datasets
+
+import icon_registration.itk_wrapper
